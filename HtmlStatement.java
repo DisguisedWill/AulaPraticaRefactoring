222b9c84a8eeff99daf0da8b3c05082d0f7b0ea0
williamtelesd <wtelesdias@gmail.com>
2022-12-05 22:26:45 -0300

    Commit 17

--- HtmlStatement.java
+++ HtmlStatement.java
@@ -4,15 +4,32 @@ public class HtmlStatement extends Statement {
 
     public String value(Customer aCustomer) {
         Enumeration rentals = aCustomer.getRentals();
-        String result = "<H1>Rentals for <EM>" + aCustomer.getName() +
+        String result = writeName(aCustomer);
+        result += writeMovies(rentals);
+        //add footer lines
+        result += writeFooter(aCustomer);
+      
+        return result;
+   }
+
+    public String writeName(Customer aCustomer) {
+        return "<H1>Rentals for <EM>" + aCustomer.getName() +
         "</EM></H1><P>\n";
+    }
+
+    public String writeMovies(Enumeration rentals) {
+        String result = "";
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
             //show figures for each rental
             result += each.getMovie().getTitle()+ ": " +
             String.valueOf(each.getCharge()) + "<BR>\n";
         }
-        //add footer lines
+        return result;
+    }
+
+    public String writeFooter(Customer aCustomer) {
+        String result = "";
         result += "<P>You owe <EM>" +
         String.valueOf(aCustomer.getTotalCharge()) + "</EM><P>\n";
         result += "On this rental you earned <EM>" + 
--- Statement.java
+++ Statement.java
@@ -1,3 +1,7 @@
-public class Statement {
+import java.util.Enumeration;
 
+public abstract class Statement {
+    public abstract String writeName(Customer aCustomer);
+    public abstract String writeMovies(Enumeration rentals);
+    public abstract String writeFooter(Customer aCustomer);
 }
